af4820c89460b84e15ab68169298dc1af8620d27
Soham Datta <splint3r777@gmail.com>
2026-06-22 00:26:05 +0530

    completed the tip calculator

--- 002/tip_calculator.py
+++ 002/tip_calculator.py
@@ -3,25 +3,24 @@ print("This is a TIP CALCULATOR")
 print("=== === === === ===")
 
 # What was the total bill?
-
-total_bill = input("What is the total bill?\n")
+total_bill = round(float(input("What is the total bill?\n")), 2)
 
 # How much percent tip would yiu like to give?
-
-tip_percent = input("How much percent tip is applied?\n")
+tip_percent = round(float(input("How much percent tip is applied?\n")), 2)
 
 # How many people to split the bill?
-
-total_people = input("How many people should split the bill?\n")
+total_people = round(float(input("How many people should split the bill?\n")))
 
 # Check the data from input
-
-print(f"Here total bill is {total_bill}, tip percentage is {tip_percent} and there are {total_people} people available")
+print(f"Here total bill is {total_bill} doller, tip percentage is {tip_percent} and there are {total_people} people available")
 
 # Develop the logic behind it.
+# print(type(total_bill))
+# print(type(total_people))
+# print(type(tip_percent))
 
-the_tip = round((total_bill * 100 / tip_percent), 2)
-final_tip = total_bill + the_tip
+the_tip = round(float(total_bill * tip_percent / 100))
+final_tip = round(float(total_bill + the_tip), 2)
 
 # Each person should pay: Result
 
