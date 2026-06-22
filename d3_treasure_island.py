27a94df985c7f206f2d75597a9a57ab43c2c0e2d
Soham Datta <splint3r777@gmail.com>
2026-06-23 01:55:07 +0530

    code correction

--- d3_treasure_island.py
+++ d3_treasure_island.py
@@ -7,13 +7,13 @@ print("Your mission is to find the Treasure.\n")
 print("You are at a cross road. Where do you want to go?")
 direction = input("Type 'left' or 'right'\n")
 
-if direction == "left":
+if direction == "left" or direction == "Left":
     print("Great. There is an island in the middle of the lake. You want to 'wait' or 'swim'?")
     stop_go = input("Type 'wait or 'swim'\n")
-    if stop_go == "wait":
+    if stop_go == "wait" or stop_go == "Wait":
         print("Now there are 3 doors in front of you. The RED one | The BLUE one | The YELLOW one")
         choose_door = input("Choose one door. Type 'red', 'blue' or 'yellow'\n")
-        if choose_door == "red" or choose_door == "blue":
+        if choose_door == "red" or choose_door == "Red" or choose_door == "blue" or choose_door == "Blue":
             print("Game Over")
         else:
             print("YEAH YOU WON!!!")
@@ -21,18 +21,3 @@ if direction == "left":
         print("Game Over")
 else:
     print("Game Over")
-
-
-
-
-
-
-
-
-#First question -> Want to go left or right?
-
-#Second question -> Want to wait or swim?
-
-#Third question -> Want red, blue or green door?
-
-
