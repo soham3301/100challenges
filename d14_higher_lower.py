0b3632e3b244e2a9558b96227ebcf42c5aa1000e
Soham Datta <splint3r777@gmail.com>
2026-07-14 01:19:34 +0530

    added ascii arts

---
+++ d14_higher_lower.py
@@ -0,0 +1,83 @@
+higher_lower_logo = '''
+▗▖ ▗▖▗▄▄▄▖ ▗▄▄▖▗▖ ▗▖▗▄▄▄▖▗▄▄▖     ▗▖    ▗▄▖ ▗▖ ▗▖▗▄▄▄▖▗▄▄▖ 
+▐▌ ▐▌  █  ▐▌   ▐▌ ▐▌▐▌   ▐▌ ▐▌    ▐▌   ▐▌ ▐▌▐▌ ▐▌▐▌   ▐▌ ▐▌
+▐▛▀▜▌  █  ▐▌▝▜▌▐▛▀▜▌▐▛▀▀▘▐▛▀▚▖    ▐▌   ▐▌ ▐▌▐▌ ▐▌▐▛▀▀▘▐▛▀▚▖
+▐▌ ▐▌▗▄█▄▖▝▚▄▞▘▐▌ ▐▌▐▙▄▄▖▐▌ ▐▌    ▐▙▄▄▖▝▚▄▞▘▐▙█▟▌▐▙▄▄▖▐▌ ▐▌
+'''
+
+the_vs_logo = '''
+▗▖  ▗▖ ▗▄▄▖
+▐▌  ▐▌▐▌   
+▐▌  ▐▌ ▝▀▚▖
+ ▝▚▞▘ ▗▄▄▞▘
+  
+'''
+
+celeb_data = [
+    {
+        "name": "Cristiano Ronaldo",
+        "follower_count": 671,
+        "description": "Footballer",
+        "country": "Portugal",
+    },
+    {
+        "name": "Leonel Messi",
+        "follower_count": 511,
+        "description": "Footballer",
+        "country": "Argentina",
+    },
+    {
+        "name": "Selena Gomez",
+        "follower_count": 415,
+        "description": "Musician",
+        "country": "United States",
+    },
+    {
+        "name": "Kylie Jenner",
+        "follower_count": 391,
+        "description": "Media Personality",
+        "country": "United States",
+    },
+    {
+        "name": "Dwayne 'The Rock' Johnson",
+        "follower_count": 390,
+        "description": "Actor/Wrestler",
+        "country": "United States",
+    },
+    {
+        "name": "Ariana Grande",
+        "follower_count": 372,
+        "description": "Musician / Actress",
+        "country": "United States",
+    },
+    {
+        "name": "Kim Kardashian",
+        "follower_count": 353,
+        "description": "Media Personality",
+        "country": "United States",
+    },
+    {
+        "name": "Virat Kohli",
+        "follower_count": 275,
+        "description": "Cricketer",
+        "country": "India",
+    },
+    {
+        "name": "Justin Bieber",
+        "follower_count": 295,
+        "description": "Musician",
+        "country": "Canada",
+    },
+    {
+        "name": "Rihana",
+        "follower_count": 146,
+        "description": "Musician",
+        "country": "Barbados",
+    },
+]
+
+"""
+Justin Bieber (Musician) – Canada - 295
+"""
+print(higher_lower_logo)
+print(the_vs_logo)
