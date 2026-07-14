41c81b582cc5a3821359075861a0bbce22058a7d
Soham Datta <splint3r777@gmail.com>
2026-07-14 17:20:11 +0530

    Higher Lower Complete - Self Made Algorithm

--- d14_higher_lower.py
+++ d14_higher_lower.py
@@ -1,3 +1,8 @@
+import random
+#? NOTE:- Only 10 celebrities are added. Real Game contains much more.
+
+#* =======================   GLOBALS   =========================
+
 higher_lower_logo = '''
 ▗▖ ▗▖▗▄▄▄▖ ▗▄▄▖▗▖ ▗▖▗▄▄▄▖▗▄▄▖     ▗▖    ▗▄▖ ▗▖ ▗▖▗▄▄▄▖▗▄▄▖ 
 ▐▌ ▐▌  █  ▐▌   ▐▌ ▐▌▐▌   ▐▌ ▐▌    ▐▌   ▐▌ ▐▌▐▌ ▐▌▐▌   ▐▌ ▐▌
@@ -76,41 +81,99 @@ celeb_data = [
     },
 ]
 
-celeb_copy = celeb_data.copy()
+#* =======================   DISPLAYS   =========================
 
-def display_compare_a():
-    print("Compare A: Cristiano Ronaldo, a Footballer, from Portugal.")
-    return "followers of cr"
+def display_compare_a(celeb_name):
+    celeb_a_followers = celeb_name["follower_count"]
+    print(f"Compare A: {celeb_name["name"]}, a {celeb_name["description"]}, from {celeb_name["country"]}.")
+    return celeb_a_followers
 
-def display_compare_b():
-    print("Compare B: Rihana, a Musician, from Barbados.")
-    return "followers of rh"
+def display_compare_b(celeb_name):
+    celeb_b_followers = celeb_name["follower_count"]
+    print(f"Compare B: {celeb_name["name"]}, a {celeb_name["description"]}, from {celeb_name["country"]}.")
+    return celeb_b_followers
 
-def display_board():
+def display_board(celeb_name_a, celeb_name_b):
     print(higher_lower_logo)
-    f1 = display_compare_a()
+    f1 = display_compare_a(celeb_name_a)
     print(the_vs_logo)
-    f2 = display_compare_b()
+    f2 = display_compare_b(celeb_name_b)
     return f1, f2
+
+#* =======================   CALCULATIONS   =========================
+
+def result_calculator(number_of_a, number_of_b, user_choice):
+    if user_choice == "a":
+        return calc_helper(number_of_a, number_of_b, user_choice, "a")
+    elif user_choice == "b":
+        return calc_helper(number_of_b, number_of_a, user_choice, "b")
+    else:
+        print("Invalid Input | Try Again")
+        return True
+
+def calc_helper(comp_a, comp_b, usr_input, comparable):
+    if usr_input == comparable:
+        user_choosen = comp_a
+        if user_choosen > comp_b:
+            return True
+        else:
+            return False
+
+#* =======================   HELPERS   =========================
+
+def celeb_picker(celeb_list, holder_adder):
+    celeb_b_new = random.choice(celeb_list)
+    holder_adder.append(celeb_b_new)
+    celeb_a_new = holder_adder.pop(0)
+    celeb_list.remove(celeb_b_new)
+    return celeb_a_new, celeb_b_new
+
+def first_celeb_picker(celeb_list, holder_adder):
+    celeb_a = random.choice(celeb_list)
+    celeb_list.remove(celeb_a)
+    celeb_b = random.choice(celeb_list)
+    celeb_list.remove(celeb_b)
+    holder_adder.append(celeb_b)
+    return celeb_a, celeb_b
 
 def ask_user():
     user_choice = input("Who has more followers? Type 'A' or 'B': ").lower()
     return user_choice
 
-def result_calculator(number_of_a, number_of_b, user_choice):
-    print(number_of_a, number_of_b, user_choice)
-    if user_choice == "a":
-        return True
+def followers_display_for_result(first_celeb, second_celeb):
+    if first_celeb["follower_count"] > second_celeb["follower_count"]:
+        print(f"{first_celeb["name"]} has {first_celeb["follower_count"]} million followers | {second_celeb["name"]} has only {second_celeb["follower_count"]} million.")
     else:
-        return False
+        print(f"{second_celeb["name"]} has {second_celeb["follower_count"]} million followers | {first_celeb["name"]} has only {first_celeb["follower_count"]} million.")
+
+#! =======================   MAIN   =========================
 
 def main():
     while True:
-        comp_a_number, comp_b_number = display_board()
-        user_input = ask_user()
-        game_stopper = result_calculator(comp_a_number, comp_b_number, user_input)
-        if not game_stopper:
-            print("You Lost.")
+        celeb_copy = celeb_data.copy()
+        celeb_holder = []
+        score = -1
+        max_attempt = 10
+        while True:
+            if len(celeb_holder) == 0:
+                celeb_for_a, celeb_for_b = first_celeb_picker(celeb_copy, celeb_holder)
+            else:
+                celeb_for_a, celeb_for_b = celeb_picker(celeb_copy, celeb_holder)
+            comp_a_number, comp_b_number = display_board(celeb_for_a, celeb_for_b)
+            user_input = ask_user()
+            game_stopper = result_calculator(comp_a_number, comp_b_number, user_input)
+            score += 1
+            max_attempt -= 1
+            if not game_stopper:
+                print(f"You Lost. Your Final Score: {score}")
+                followers_display_for_result(celeb_for_a, celeb_for_b)
+                break
+            if max_attempt == 0:
+                print(f"Thanks for completing the game. You WON.")
+                break
+        play_again = input("Want to restart the game? Y / N\n").lower() == "y"
+        if not play_again:
+            print("Thanks for playing The Higher Lower Game. See You.")
             break
 
 main()
