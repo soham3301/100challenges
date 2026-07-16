9ea2822b1af996f2a2812093039e64a2f4f74b3e
Soham Datta <splint3r777@gmail.com>
2026-07-17 00:08:16 +0530

    OOP Coffee Machine - more refactored

--- d16_oop_coffee_machine/main.py
+++ d16_oop_coffee_machine/main.py
@@ -6,25 +6,20 @@ drinks_menu = Menu()                    #! menu / get_items() / find_drink(order
 coffee_maker = CoffeeMaker()            #! report() / is_resource_sufficient(drink) / make_coffee(order)
 money_machine = MoneyMachine()          #! report() / process_coins() / make_payment(cost)
 
-
-def display_and_user_input():
-    return input(f"What would you like? ({drinks_menu.get_items()}): ")
-
 def main():
     while True:
-        user_choice = display_and_user_input()
+        user_choice = input(f"What would you like? ({drinks_menu.get_items()}): ")
         if user_choice == "exit":
             print("Thanks for using Coffee Machine.")
             break
         elif user_choice == "report":
             coffee_maker.report()
             money_machine.report()
-        the_drink = drinks_menu.find_drink(user_choice)
-        if the_drink:
-            if coffee_maker.is_resource_sufficient(the_drink):
-                if money_machine.make_payment(the_drink.cost):
-                    coffee_maker.make_coffee(the_drink)
         else:
-            print("Invalid Input")
+            the_drink = drinks_menu.find_drink(user_choice)
+            if the_drink:
+                if coffee_maker.is_resource_sufficient(the_drink):
+                    if money_machine.make_payment(the_drink.cost):
+                        coffee_maker.make_coffee(the_drink)
 
 main()
