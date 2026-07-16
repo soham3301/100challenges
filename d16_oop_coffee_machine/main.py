b1685d50036b009da07a4bd85dfa3fed90119b62
Soham Datta <splint3r777@gmail.com>
2026-07-16 21:31:24 +0530

    OOP Coffee Machine - Done

--- d16_oop_coffee_machine/main.py
+++ d16_oop_coffee_machine/main.py
@@ -1,4 +1,30 @@
-import prettytable
-import logos
+from menu import Menu
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
 
-print(logos.admin_logo)
+drinks_menu = Menu()                    #! menu / get_items() / find_drink(order_name)
+coffee_maker = CoffeeMaker()            #! report() / is_resource_sufficient(drink) / make_coffee(order)
+money_machine = MoneyMachine()          #! report() / process_coins() / make_payment(cost)
+
+
+def display_and_user_input():
+    return input(f"What would you like? ({drinks_menu.get_items()}): ")
+
+def main():
+    while True:
+        user_choice = display_and_user_input()
+        if user_choice == "exit":
+            print("Thanks for using Coffee Machine.")
+            break
+        elif user_choice == "report":
+            coffee_maker.report()
+            money_machine.report()
+        the_drink = drinks_menu.find_drink(user_choice)
+        if the_drink:
+            if coffee_maker.is_resource_sufficient(the_drink):
+                if money_machine.make_payment(the_drink.cost):
+                    coffee_maker.make_coffee(the_drink)
+        else:
+            print("Invalid Input")
+
+main()
