16f1c1bfbfa20576ab87c1ef77117f7b5dc2033a
Soham Datta <splint3r777@gmail.com>
2026-07-16 14:28:29 +0530

    OOP - the coffee machine mods

--- d16_oop_coffee_machine/coffee_machine.py
+++ d16_oop_coffee_machine/coffee_machine.py
@@ -3,3 +3,45 @@ machine_storage = {
     "coffee":300,
     "milk":900,
 }
+
+ingredients_purchase_cost = {
+    "water":{
+        "price":32,
+        "quantity":100,
+    },
+    "coffee":{
+        "price":55,
+        "quantity":100,
+    },
+    "milk":{
+        "price":72,
+        "quantity":100,
+    },
+}
+
+drinks = {
+    "espresso":{
+        "ingredients":{
+            "water":50,
+            "coffee":18,
+            "milk":0
+        },
+        "price":90,
+    },
+    "latte":{
+        "ingredients":{
+            "water":200,
+            "coffee":24,
+            "milk":150,
+        },
+        "price":180,
+    },
+    "cappuccino":{
+        "ingredients":{
+            "water":250,
+            "coffee":24,
+            "milk":100,
+        },
+        "price":280,
+    },
+}
