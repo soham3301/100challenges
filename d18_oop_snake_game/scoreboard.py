b814ccc26343f090776249df938acb0bc9225c14
Soham Datta <splint3r777@gmail.com>
2026-08-07 16:42:30 +0530

    18 - snake game with file system record

--- d18_oop_snake_game/scoreboard.py
+++ d18_oop_snake_game/scoreboard.py
@@ -8,7 +8,8 @@ class Scoreboard(Turtle):
     def __init__(self, shape = "square", undobuffersize = 1000, visible = True):
         super().__init__(shape, undobuffersize, visible)
         self.score = 0
-        self.high_score = 0
+        with open("data.txt") as high_score:
+            self.high_score = int(high_score.read())
         self.hideturtle()
         self.penup()
         self.color("white")
@@ -22,6 +23,8 @@ class Scoreboard(Turtle):
     def reset_high_score(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", mode="w") as highest_score:
+                highest_score.write(str(self.high_score))
         self.score = 0
         self.write_score()
 
